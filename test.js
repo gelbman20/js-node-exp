89527b4a23f8499f7c1443325fcbabd08d54d28f
gelbman20 <gelbman20@gmail.com>
2020-05-30 16:42:27 +0300

    build lite express server demo

--- test.js
+++ test.js
@@ -1,14 +1,36 @@
-let http = require('http')
+let express = require('express')
+let ourApp = express()
 
-let ourApp = http.createServer(function (req, res) {
-	if (req.url === '/') {
-		res.end('Hello, and welcome to our website.')
-	}
+ourApp.use(express.urlencoded({
+  extended: false
+}))
 
-	if (req.url === '/about') {
-		res.end('Thank you for the interest in our company.')
-	}
-
-	res.end('We cannot the page you are looking for.')
+ourApp.get('/', function (req, res) {
+  res.send(`
+    <form action="/answer" method="POST">
+      <p>What color is the sky on a clear and sunny day?</p>
+      <input type="text" name="skyColor" autocomplete="off">
+      <button>Submit Answer</button>
+    </form>
+  `)
 })
+
+ourApp.post('/answer', function (req, res) {
+  if (req.body.skyColor.toLowerCase() == 'blue') {
+    res.send(`
+      <p>Congrats, that is the correct answer!</p>
+      <a href="/">Back to homepage</a>
+    `)
+  } else {
+    res.send(`
+      <p>Sorry, that is incorrect</p>
+      <a href="/">Back to homepage</a>
+    `)
+  }
+})
+
+ourApp.get('/answer', function (req, res) {
+  res.send('Are you lost? There is nothing to see here.')
+})
+
 ourApp.listen(3000)
