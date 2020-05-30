14df072948931596708c963dfed3104339437c87
gelbman20 <gelbman20@gmail.com>
2020-05-30 16:16:50 +0300

    test work with node.js

---
+++ test.js
@@ -0,0 +1,14 @@
+let http = require('http')
+
+let ourApp = http.createServer(function (req, res) {
+	if (req.url === '/') {
+		res.end('Hello, and welcome to our website.')
+	}
+
+	if (req.url === '/about') {
+		res.end('Thank you for the interest in our company.')
+	}
+
+	res.end('We cannot the page you are looking for.')
+})
+ourApp.listen(3000)
